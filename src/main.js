9ee6ad231e66a7b4facb97b1181c327f9b79c339
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-31 17:20:42 -0300

    Comentários sobre o arquivo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue' //GLOBAL VIEW OBJECT VUE sendo importado no módulo
+import App from './App.vue' //COMPONENTE
 
 Vue.config.productionTip = false
 
+//INSTANCIA DA VUE
+//criada a partir do global view object 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
