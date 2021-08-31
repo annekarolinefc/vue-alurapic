ba5676b1207b2d9aaf800fe09770b8c15ef957e4
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-31 17:20:26 -0300

    Trabalhando data binding e diretiva v-for

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--Necessidade de encapsular dentro de uma div-->
+  <div class="container">
+    <h1>
+      <!--
+        Interpolação
+        ou usar
+        <h1 v-text="titulo"></h1>
+        -->
+      {{titulo}}
+    </h1>
+    <!--
+    Proibido usar interpolação em atributos. 
+    usar v-bind:  
+    um atalho é usar apenas dois pontos(:) 
+    :src
+    -->
+  <ul>
+    <!--DIRETIVAS-->
+    <li v-for="foto in fotos" :key="foto">
+      <img :src="foto.url" :alt="foto.titulo">
+    </li>
+  </ul>
+
   </div>
-</template>
+  </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      titulo: "Alurapic",
+      fotos: [{
+        url: 'https://s2.glbimg.com/slaVZgTF5Nz8RWqGrHRJf0H1PMQ=/0x0:800x450/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/U/e/NTegqdSe6SoBAoQDjKZA/cachorro.jpg',
+        titulo: 'cachorro'
+        },
+        {
+        url: 'https://s2.glbimg.com/slaVZgTF5Nz8RWqGrHRJf0H1PMQ=/0x0:800x450/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2019/U/e/NTegqdSe6SoBAoQDjKZA/cachorro.jpg',
+        titulo: 'cachorro 2'
+        }
+      ],
+    }
   }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
